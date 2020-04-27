fa569ba982c5a05cbc1805ebbf508cca92728f58
Max Farver <mfarver99@gmail.com>
2020-04-26 22:06:35 -0500

    try it again

--- components/common/sections/SectionHeader.js
+++ components/common/sections/SectionHeader.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const SectionHeader = ({ children, size }) => {
+const SectionHeader = ({ children, size = null }) => {
   return (
     <div>
       <h2 className={`section-header ${size || ""}`}>{children}</h2>
