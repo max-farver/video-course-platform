5fc91a1dd055c4d22d36cc550cb5a83bd0c27471
Max Farver <mfarver99@gmail.com>
2020-04-27 10:15:20 -0500

    style #__next in a way that works in prod

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,16 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Alegreya+SC:ital,wght@0,400;0,700;1,400&family=Cabin:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          <style>
+            {`
+            #__next {
+              display: flex;
+              flex-direction: column;
+              min-height: 100vh;
+              justify-content: space-between;
+            }
+            `}
+          </style>
         </Head>
         <body>
           <Main />
